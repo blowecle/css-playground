ae738905a5abafb50753e5d54e15e012f1d5e886
Brandon Lowe <BLoweCLE@gmail.com>
2023-01-05 19:05:11 -0600

    added transistions

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Transforms from './transforms/Transforms';
 import Gallery from './polaroid/Gallery';
+import Transition from './transitions/Transition.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Gallery/>
+    <Transition></Transition>
   </React.StrictMode>
 );
---
+++ src/transitions/Transition.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './transition.css';
+
+const Transition = () => {
+    return (
+        <div className='transition-div'>Hello Jane</div>
+    )
+}
+
+export default Transition;
